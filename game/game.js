a1b91ba0cbb1989a8b6770e91236615cbc80cf7e
Emily Eisenberg <xymostech@gmail.com>
2014-06-07 15:14:29 -0600

    Make a game-like thing to demonstrate new features

--- game/game.js
+++ game/game.js
@@ -4,5 +4,57 @@ window.addEventListener("load", function() {
     var canvas = document.getElementById("game");
     var game = gamelib.setup(canvas);
 
-    //game.start();
+    window.game = game;
+
+    var Rotater = g.object.Rectangle.extend({
+        init: function rotaterInit(options) {
+            this._super(options);
+
+            this.rotate = true;
+        },
+
+        update: function(timeDelta) {
+            this._super(timeDelta);
+
+            if (this.rotate) {
+                this.rotation += Math.PI / 2 * timeDelta;
+            }
+        }
+    });
+
+    window.a = new Rotater({
+        x: 400, y: 300,
+        width: 100, height: 100,
+        color: "#f00"
+    });
+
+    window.b = new g.object.Circle({
+        x: 200, y: 0,
+        radius: 50,
+        color: "#0f0"
+    });
+
+    window.c = new g.object.Rectangle({
+        x: 0, y: 200,
+        width: 100, height: 100,
+        color: "#00f"
+    });
+
+    game.root.addChild(window.a);
+    window.a.addChild(window.b);
+    window.b.addChild(window.c);
+
+    game.keys.setControlSet({
+        "a": "hello"
+    });
+
+    game.keys.on("key-A", function(data) {
+        console.log("Pressed!", data);
+    });
+
+    game.keys.on("control-hello", function(data) {
+        console.log("Hello!", data);
+    });
+
+    game.start();
 });
